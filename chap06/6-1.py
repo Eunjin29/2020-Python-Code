6395339f906442c5ea2d9fe18478e6878dcf36be
Eunjin29 <64787419+Eunjin29@users.noreply.github.com>
2020-05-14 01:14:21 +0900

    Add files via upload

---
+++ chap06/6-1.py
@@ -0,0 +1,3 @@
+groupnumber = {'잔나비':5,'트와이스':9,'방탄소년단':7}
+print(groupnumber)
+print(type(groupnumber))
---
+++ chap06/6-7.py
@@ -0,0 +1,11 @@
+planets = set('해달별')
+friuts = set(['감','귤'])
+nuts = {'밤','잣'}
+things = {('밤','잣'),('감','귤'),'해달'}
+# things = {['밤','잣'],('감','귤'),'해달'}  #오류발생
+
+
+print(planets)
+print(friuts)
+print(nuts)
+print(things)
